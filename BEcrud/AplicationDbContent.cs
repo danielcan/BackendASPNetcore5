63165760debaf8e18120a94a886f05909ec616bf
Daniel <danielcanc@gmail.com>
2022-11-12 17:42:45 -0600

    First backend asp.net core 5

---
+++ BEcrud/AplicationDbContent.cs
@@ -0,0 +1,21 @@
+﻿using BEcrud.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BEcrud
+{
+    public class AplicationDbContent: DbContext
+    {
+        public DbSet<Comentario> Comentario { get; set; }
+
+        public AplicationDbContent(DbContextOptions<AplicationDbContent> options) : base(options)
+        {
+
+        }
+
+
+    }
+}
